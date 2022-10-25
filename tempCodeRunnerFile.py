92eb402c78b671ef2bce681c033ff9bf0886d112
Pius <piusemmanuelchukwuemeka@gmail.com>
2022-10-25 22:29:25 +0100

    added an inputFunctionExercise.py file

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+"""Write a program that prints the number of characters in a user's name. 
+"""
+
+
+print(len(input("What is your name? ")))
+
+print("Hello " + input("What's your other name please: "))   
+
+
