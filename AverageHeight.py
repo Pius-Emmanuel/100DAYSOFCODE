d770654d2947bd4fb1972b088d6ca1e70f27ee4e
Pius-Emmanuel <piusemmanuelchukwuemeka@gmail.com>
2022-11-11 13:33:55 +0100

    added AverageHeight.py

---
+++ AverageHeight.py
@@ -0,0 +1,7 @@
+"""
+program that calculates the average student height from a List of heights.
+"""
+
+student_height = input("Input a list of student heights: ").split()
+
+for student in range(0)
