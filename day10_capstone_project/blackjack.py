59e880a1853cb04fbff84e4691561e69f8b7a639
Pius <piusemmanuelchukwuemeka@gmail.com>
2022-12-01 23:23:38 +0100

    add art.py

--- day10_capstone_project/blackjack.py
+++ day10_capstone_project/blackjack.py
@@ -2,13 +2,14 @@
 """
 input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-if score == dealer_score:
-    return "push"
-elif score > dealer_score and score > 21:
-    return "You lose!"
-elif score > dealer_score and score < 22:
-    return "You win!"
-elif score < dealer_score and dealer_score < 22:
-    return "You lose!"
-elif score < dealer_score and dealer_score > 21:
-    return "You win!"
+#if score == dealer_score:
+#    return "push"
+#elif score > dealer_score and score > 21:
+#    return "You lose!"
+#elif score > dealer_score and score < 22:
+#    return "You win!"
+#elif score < dealer_score and dealer_score < 22:
+#    return "You lose!"
+#elif score < dealer_score and dealer_score > 21:
+#    return "You win!"
+print("ok I'm really trying  hard here")
