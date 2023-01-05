b20aebf44a925be1fbf996cd871ea824389a1fb8
Pius <piusemmanuelchukwuemeka@gmail.com>
2023-01-05 19:57:03 +0100

    fix shapes.py

--- day18/shapes.py
+++ day18/shapes.py
@@ -3,6 +3,7 @@ import random
 
 yu = t.Turtle()
 
+
 colours = ["deep pink","navy","dark cyan","tomato","black","red","blue","brown"]
 
 def shape(num_of_sides):
@@ -11,6 +12,7 @@ def shape(num_of_sides):
         yu.forward(100)
         yu.right(angle)
 
+
 for sides in range(3, 11):
     yu.color(random.choice(colours))
-    shape(sides)
+    shape(sides)        
