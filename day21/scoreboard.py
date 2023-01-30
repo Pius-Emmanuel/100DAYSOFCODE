ec81c7fd41aadd64486dc9a3eb3c9e13723eba4d
Pius <piusemmanuelchukwuemeka@gmail.com>
2023-01-30 23:27:11 +0100

    Annotation fix

--- day21/scoreboard.py
+++ day21/scoreboard.py
@@ -4,12 +4,11 @@ ALIGNMENT = "center"
 FONT = ("courier", 15, "normal")
 
 
-
 class Scoreboard(Turtle):
-    
+
     def __init__(self):
         super().__init__()
-        self.score =0
+        self.score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -17,10 +16,9 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"score: {self.score}", align= ALIGNMENT, font= FONT)
-
+        self.write(f"score: {self.score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
-        self.score +=1
+        self.score += 1
         self.clear()
         self.update_scoreboard()
