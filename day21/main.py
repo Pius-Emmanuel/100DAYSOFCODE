b4b788d124ae009b936359866ac977df7b965f4a
Pius <piusemmanuelchukwuemeka@gmail.com>
2023-01-30 23:24:21 +0100

    minor fix

--- day21/main.py
+++ day21/main.py
@@ -15,7 +15,7 @@ food = Food()
 scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(snake.up,  "Up")
+screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
@@ -27,12 +27,9 @@ while game_is_on:
 
     snake.move()
 
-    #Detect collision with food.
+    # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.increase_score()
-        
-
-
 
 screen.exitonclick()
