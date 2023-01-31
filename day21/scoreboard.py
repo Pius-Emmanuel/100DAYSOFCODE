b7647ce32bfb066087b99ce5437346b62f707ce8
Pius <piusemmanuelchukwuemeka@gmail.com>
2023-01-31 13:33:49 +0100

    modify scoreboard

--- day21/scoreboard.py
+++ day21/scoreboard.py
@@ -1,7 +1,6 @@
 from turtle import Turtle
-
 ALIGNMENT = "center"
-FONT = ("courier", 15, "normal")
+FONT = ("Courier", 15, "normal")
 
 
 class Scoreboard(Turtle):
@@ -16,7 +15,11 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
