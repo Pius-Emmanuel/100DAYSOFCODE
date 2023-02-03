87fdbe15a162a1f61359bf98d8dba7a4a7d37cf1
Pius <piusemmanuelchukwuemeka@gmail.com>
2023-02-03 23:53:00 +0100

    complete pong game

---
+++ day22/README.md
@@ -0,0 +1,2 @@
+# Day 22
+* Day 22 consists of a pong game
--- day22/ball.py
+++ day22/ball.py
@@ -8,9 +8,23 @@ class Ball(Turtle):
         self.shape("circle")
         self.penup()
         self.color("yellow")
-        self.width("")
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
 
     def move(self):
-        new_x = self.xcor() + 1
-        new_y = self.ycor() + 1
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+        self.move_speed *= 0.9
+
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
