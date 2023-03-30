aaa783c30587be0e8dea4172bc2295d19fbe5644
Pius <piusemmanuelchukwuemeka@gmail.com>
2023-03-30 01:22:00 +0100

    finished us_state_game

--- day25/us_states_game/main.py
+++ day25/us_states_game/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas as pd
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,9 +7,32 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-# def get_mouse_click_cor(x, y):
-#     print(x, y)
-#
-#
-# turtle.onscreenclick(get_mouse_click_cor)
-# turtle.mainloop()
+data = pd.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt = "What's another states name?").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+# if answer_state is one of the states in all the states of the 50_states.csv
+    # if they got it right:
+        #create a turtle to write the name of the state at the state's x and y coordinates
