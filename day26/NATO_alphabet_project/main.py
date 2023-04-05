19beca71fa9b8b4251d5511f1bc00616922a5cc4
Pius <piusemmanuelchukwuemeka@gmail.com>
2023-04-05 11:31:27 +0100

    finished main.py

--- day26/NATO_alphabet_project/main.py
+++ day26/NATO_alphabet_project/main.py
@@ -3,3 +3,6 @@ import pandas as pd
 nato_df = pd.read_csv("nato_phonetic_alphabet.csv")
 new_df = {row.letter: row.code for (index, row ) in nato_df.iterrows()}
 print(new_df)
+word = input("Enter a word: ").upper()
+output_list = [new_df[letter] for letter in word]
+print(output_list)
